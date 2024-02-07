56aff8677addda56a6cce421d55c9cf62d06eed0
Patricio Inzaghi <51960393+pinzaghi@users.noreply.github.com>
2024-02-07 14:50:28 +0100

    contract v1 finished

--- backend/src/FreeNearMe.sol
+++ backend/src/FreeNearMe.sol
@@ -7,8 +7,16 @@ pragma solidity ^0.8.22;
 */
 contract FreeNearMe {
 
-    /** @dev The denominator for the latitude and longitude int64 coordinates. */
-    uint64 constant RESOLUTION = 100000000000000;
+    /** @dev The denominator for the latitude and longitude int64 coordinates. 
+     *  We obtain the lat/long values by doing VALUE/COORD_PRECISION
+    */
+    int64 constant COORD_PRECISION = 1000000; // 6 decimal degrees = 111mm precision
+    /** @dev Used to compare the received lat/long value with the truncated value
+     *  with one decimal precision, e.g., truncate(180000000) = 1800.
+    */
+    int64 constant TRUNCATE = 100000;
+    int64 constant MAX_LAT = 90000000; // 90.000000
+    int64 constant MAX_LONG = 180000000; // 180.000000
 
     /**
     * @dev Structure for a service
@@ -22,8 +30,10 @@ contract FreeNearMe {
         uint256 serviceId;
         ServiceType kind;
         string desc;
-        int64 lat;
-        int64 long;
+        int32 lat;
+        int32 long;
+        int16 citylat;
+        int16 citylong;
     }
 
     /**
@@ -40,18 +50,31 @@ contract FreeNearMe {
         Other
     }
 
+    /**
+    * @dev Structure for a service update
+    * @param serviceId ID of the service
+    * @param kind Service type
+    */
     struct Update {
         string desc;
+        uint256 timestamp;
     }
 
     /// @dev Minimal amount of sponsores required to do writes to the contract.
     uint minSponsors;
     /// @notice Sponsors that a user has. If it has enough it can submit/update services.
-    mapping (address => address[]) sponsors;
+    mapping (address => uint256) sponsorsReceived;
+    /// @dev Fast way to find if address1 sponsored address2 (in that order in the mapping).
+    mapping (address => mapping(address => bool)) sponsorsGiven;
     /// @notice Submited services.
     Service[] services;
     /// @notice Updates to the services, e.g., bathroom out of order.
+    /// @dev Map serviceId => Array of updates
     mapping (uint256 => Update[]) servicesUpdates;
+
+    /// @dev citylat and citylong are coordinates truncated to first decimal, this means aprox 11km precision 
+    event ServiceRegistered(uint serviceId, int64 lat, int64 long, int16 indexed citylat, int16 indexed citylong, ServiceType indexed kind);
+    event ServiceUpdate(uint indexed serviceId, string desc);
     
     /// @dev We start the contract with a seed of trusted users.
     constructor(uint8 _minSponsors, address[] memory initialAddresses) {
@@ -64,8 +87,86 @@ contract FreeNearMe {
             address addressToSponsor = initialAddresses[i];
             for(uint j=0; j < _minSponsors; j++){
                 address sponsorAddress = initialAddresses[j];
-                sponsors[addressToSponsor].push(sponsorAddress);
+                _sponsorUser(addressToSponsor, sponsorAddress);
             }
         }
+    }
+
+    function _sponsorUser(address addressToSponsor, address sponsorAddress) internal {
+        require(addressToSponsor != address(0), "Invalid address.");
+        require(sponsorAddress != address(0), "Invalid address.");
+        sponsorsReceived[addressToSponsor]++;
+        sponsorsGiven[sponsorAddress][addressToSponsor] = true;
+    }
+
+    /**
+    * @dev Modifier to check if the user has been coopted.
+    */
+    modifier onlySponsored() {
+        require(sponsorsReceived[msg.sender] >= minSponsors, "You're not sponsored by enough users to perform this action.");
+        _;
+    }
+
+    /**
+    * @dev Get the service data by its ID
+    * @param _id Service ID
+    * @return Service Structure with the service information
+    */
+    function getService(uint256 _id) external view returns (Service memory) {
+        require(_id < services.length, "Invalid service ID.");
+        return services[_id];
+    }
+
+    /**
+    * @dev Submit a new service.
+    * @param kind The type of service to be submited.
+    * @param desc Description of the service, optional.
+    * @param lat Latitude coordinate of the service.
+    * @param long Longitude coordinate of the service.
+    */
+    function submitService(ServiceType kind, string memory desc, int32 lat, int32 long) external onlySponsored {
+        require(lat <= MAX_LAT && lat >= -MAX_LAT, "Invalid latitude value.");
+        require(long <= MAX_LONG && long >= -MAX_LONG, "Invalid longitude value.");
+
+        int16 citylat = int16(lat / TRUNCATE);
+        int16 citylong = int16(long / TRUNCATE);
+
+        Service memory service;
+        service.desc = desc;
+        service.kind = kind;
+        service.lat = lat;
+        service.long = long;
+        service.citylat = citylat;
+        service.citylong = citylong;
+        services.push(service);
+
+        emit ServiceRegistered(services.length-1, lat, long, citylat, citylong, kind);
+    }
+
+    /**
+    * @dev Submit an update to a service.
+    * @param id The ID of service to be updated.
+    * @param desc Description of the update.
+    * @param timestamp UNIX timestamp of the update.
+    */
+    function submitServiceUpdate(uint256 id, string memory desc, uint256 timestamp) external onlySponsored {
+        require(id < services.length, "Invalid service ID.");
+        require(keccak256(abi.encode(desc)) != keccak256(abi.encode("")), "Update description cannot be empty.");
+
+        Update memory update;
+        update.desc = desc;
+        update.timestamp = timestamp;
+        servicesUpdates[id].push(update);
+
+        emit ServiceUpdate(id, desc);
+    }
+
+    /**
+    * @dev Sponsor a user.
+    * @param addressToSponsor Address of the user to sponsor.
+    */
+    function sponsorUser(address addressToSponsor) external onlySponsored {
+        require(sponsorsGiven[msg.sender][addressToSponsor] == false, "You already sponsored this user.");
+        _sponsorUser(addressToSponsor, msg.sender);
     }
 }
