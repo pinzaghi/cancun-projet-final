23b928c6f90b57e52db4b8e0460aa97a9f77ba32
Patricio Inzaghi <51960393+pinzaghi@users.noreply.github.com>
2024-02-08 13:47:16 +0100

    deploy script finished

--- backend/script/FreeNearMe.s.sol
+++ backend/script/FreeNearMe.s.sol
@@ -2,12 +2,22 @@
 pragma solidity ^0.8.22;
 
 import {Script, console2} from "forge-std/Script.sol";
+import "../src/FreeNearMe.sol";
 
-contract FreeNearMeScript is Script {
+contract DeployScript is Script {
     function setUp() public {}
 
     function run() public {
-        vm.broadcast();
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        string memory key = "SEED_ADDRESS_VALUES";
+        string memory delimiter = ",";
+        address[] memory seed = vm.envAddress(key, delimiter);
+
+        vm.startBroadcast(deployerPrivateKey);
+
+        FreeNearMe fnm = new FreeNearMe(1, seed);
+
+        vm.stopBroadcast();
     }
 
     // add this to be excluded from coverage report
