c8cea03dc681c13b1a0345ffa70b3cde31e4762a
Patricio Inzaghi <51960393+pinzaghi@users.noreply.github.com>
2024-02-09 16:12:09 +0100

    basic tailwind + shadcn working

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,3 +1,13 @@
+import type { Metadata } from 'next'
+ 
+// These styles apply to every route in the application
+import './globals.css'
+ 
+export const metadata: Metadata = {
+  title: 'FreeNearMe App',
+  description: 'Find and share free services in your neighborhood.',
+}
+
 export default function RootLayout({
     children,
   }: {
@@ -5,6 +15,9 @@ export default function RootLayout({
   }) {
     return (
       <html lang="en">
+        <head>
+          <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
+        </head>
         <body>{children}</body>
       </html>
     )
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,3 +1,8 @@
+import { Button } from "@/components/ui/button"
+
 export default function Page() {
-    return <h1>Hello, Next.js!</h1>
+    return <>
+        <h1 className="text-3xl font-bold underline">Hello, Next.js!</h1>
+        <Button>Button</Button>
+    </>
 }
