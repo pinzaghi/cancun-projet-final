533f0bd26f787c5f6302742cf3e5dbaa9a934092
Patricio Inzaghi <51960393+pinzaghi@users.noreply.github.com>
2024-02-12 17:12:49 +0100

    style and misc changes in search component

--- frontend/components/ServicesSearchControls.tsx
+++ frontend/components/ServicesSearchControls.tsx
@@ -1,5 +1,7 @@
 'use client'
 
+import { useState } from 'react';
+
 import { 
     Users, 
     Wifi, 
@@ -13,22 +15,17 @@ import {
 
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
 
-export default function ServicesSearchControls({serviceRender}) {
+export default function ServicesSearchControls({serviceSearch}) {
 
     const serviceToDisplay = (serviceKind) => {
-        if (serviceKind == "Other") {
-
-        } else {
-            serviceRender(serviceKind);
-        } 
+        serviceSearch(serviceKind, "");
     }
 
     return (
         <div className="grid md:grid-cols-1 gap-4 mt-4">  
             <span className="font-bold">Select a service:</span>
-            <div className="grid md:grid-cols-4 gap-4">
+            <div className="grid md:grid-cols-3 gap-4">
                 <Button variant="outline" onClick={() => serviceToDisplay("Bathroom")}> <Users/> Bathroom </Button>
                 <Button variant="outline" onClick={() => serviceToDisplay("WiFi")}> <Wifi/> WiFi </Button>
                 <Button variant="outline" onClick={() => serviceToDisplay("Water")}> <GlassWater/> Water </Button>
@@ -36,7 +33,7 @@ export default function ServicesSearchControls({serviceRender}) {
                 <Button variant="outline" onClick={() => serviceToDisplay("AirPump")}> <Wind/> Air pump </Button>
                 <Button variant="outline" onClick={() => serviceToDisplay("Workstation")}> <LampDesk/> Workstation </Button>
                 <Button variant="outline" onClick={() => serviceToDisplay("ATM")}> <CircleDollarSign/> ATM </Button>
-                <Button variant="outline" onClick={() => serviceToDisplay("Charge")}> <PlugZap/> Charging station </Button>
+                <Button variant="outline" onClick={() => serviceToDisplay("ChargingStation")}> <PlugZap/> Charging station </Button>
             </div>
             <span className="font-bold">Or describe it:</span>
             <div>
