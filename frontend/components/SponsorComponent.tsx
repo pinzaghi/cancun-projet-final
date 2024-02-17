162a6c51dd37f31031a1d813719472792566403b
Patricio Inzaghi <51960393+pinzaghi@users.noreply.github.com>
2024-02-17 11:51:37 +0100

    sponsor user functionality in frontend

--- frontend/components/SponsorComponent.tsx
+++ frontend/components/SponsorComponent.tsx
@@ -5,24 +5,62 @@ import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
 import { useToast } from "@/components/ui/use-toast"
 
+import { useAccount } from 'wagmi'
+import { writeContract, waitForTransaction, getPublicClient } from '@wagmi/core'
+
+import { 
+    servicesTypes, 
+    servicesTypeIndex, 
+    contractAddress, 
+    contractBlock, 
+    abi } from '@/constants'
+
 export default function SponsorComponent() {
-    const [sponsorAddress, setSponsorAddress] = useState("");
+    const [addressToSponsor, setAddressToSponsor] = useState("");
 
     const { toast } = useToast()
 
-    function submitNewService() {
-        if(sponsorAddress===""){
+    // Viem public client déjà défini dans le layout 
+    const { address, isConnected } = useAccount();
+
+    async function sponsorAddress() {
+        if(addressToSponsor===""){
             toast({
                 variant: "destructive",
                 title: "No address specified",
                 description: "Enter the user's public address to sponsor.",
               })
-        }else{
-            toast({
-                title: sponsorAddress,
-              })
+              return;
         }
+
+        try{            
+            const { hash } = await writeContract({
+                address: contractAddress,
+                abi: abi,
+                functionName: "sponsorUser",
+                args: [
+                    addressToSponsor],
+                account: address
+            })
+            const data = await waitForTransaction({
+                hash: hash,
+            })
+
+            toast({
+                title: "Address sponsored successfully.",
+            })
+
+        } catch(error) {
+            console.log(error);
+            toast({
+                variant: "destructive",
+                title: 'Error sponsoring the address.',
+                description: error.message,
+            })
+        }
+
     }
+
 
     return (
         <>
@@ -30,11 +68,11 @@ export default function SponsorComponent() {
                 <div>
                     <div className="flex items-center">
                         <Input 
-                            value={sponsorAddress} 
+                            value={addressToSponsor} 
                             placeholder="Address to sponsor" 
                             className="mr-2"
-                            onChange = {(event)=> setSponsorAddress(event.target.value)} /> 
-                        <Button onClick={() => submitNewService()}>Submit</Button>
+                            onChange = {(event)=> setAddressToSponsor(event.target.value)} /> 
+                        <Button onClick={() => sponsorAddress()}>Submit</Button>
                     </div>
                 </div>
             </div>
